a1ea5c83d5dd87640a505265ca61331ee33bc852
Cecilia Wong <ceciliakhwong@gmail.com>
2017-10-03 16:02:56 -0400

    Ranodmly select one toy to gift, ensuring it's not a kaleidoscope. Remove gifted toy from list of toys, to list in letter

--- naughty_letter_writer.rb
+++ naughty_letter_writer.rb
@@ -12,15 +12,29 @@ kids_data.each_line do |kid|
   toys = naughty_kid_array[3..8]
 
   next unless behaviour == "naughty"
+  # random toy to receive, ensure it's not a kaleidoscope
+  is_kaleidoscope = true
+
+  while is_kaleidoscope
+    random_number = rand(1..5)
+    gift_toy = toys[random_number]
+    if gift_toy != "Kaleidoscope"
+      is_kaleidoscope = false
+    end
+  end
+
+  # remove gift toy from toys array to list in letter
+  toys.delete(gift_toy)
+
   # infraction
   kid_infraction_array = kid.split("|")
-  infraction = kid_infraction_array[1]
+  infraction = kid_infraction_array[1].strip
 
   filename = "letters/naughty/" + name + ".txt"
   letter_text = ERB.new(naughty_letter, nil, "-").result(binding)
 
   File.write(filename, letter_text)
-  puts "Letter has been written for #{ name }!"
+  # puts "Letter has been written for #{ name }!"
 
-  puts "Current line has this info: #{ name }, #{ gender }, #{ behaviour }, #{ toys }, with infraction: #{ infraction }"
+  # puts "Current line has this info: #{ name }, #{ gender }, #{ behaviour }, #{ toys }, with infraction: #{ infraction }"
 end
